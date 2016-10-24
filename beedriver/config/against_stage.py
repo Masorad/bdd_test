18b8559884a33ec642f15226898657b50eb85637
Petr Hejna <Achse0@gmail.com>
2016-10-24 13:46:31 +0200

    added against stage config

---
+++ beedriver/config/against_stage.py
@@ -0,0 +1,19 @@
+engager_url = 'http://engager-stage.brandembassy.com/'
+
+office = {
+    'url': 'http://office-stage.brandembassy.com/',
+    'login_name': 'hejna@brandembassy.com',
+    'login_password': 'TVu6N1xz',
+}
+
+livechat_url = 'https://vps-web-utils.awsbrandembassy.com/livechat-window-gherkin/'
+
+brand_id = '1073'  # Gherkin's Selenium Brand
+
+# abstract brand from this once chat window communicates with engager
+brand_status_switcher_url = 'http://localhost:3000/?testing1234'
+
+agent = {
+    'login': "gherkin@brandembassy.com",
+    'password': "ztX2ndLn",
+}
