9a006c7e6b4073481e287343a9580a4180bccd02
Petr Hejna <Achse0@gmail.com>
2016-11-10 19:10:39 +0100

    jebu to!

--- beedriver/po/engager/left_panel/livechat_button/__init__.py
+++ beedriver/po/engager/left_panel/livechat_button/__init__.py
@@ -6,4 +6,10 @@ class LiveChatButton(Button):
         self.wait_for_exist()
         button = self.find()
         status_attribute = button.get_attribute('class')
+
+        if 'online' not in status_attribute and 'offline' not in status_attribute:
+            raise Exception(
+                "Engager live-chat button has not any of 'online' nor 'offline' classes. Classes: ["
+                + ", ".join(status_attribute) + "]")
+
         return 'online' in status_attribute
