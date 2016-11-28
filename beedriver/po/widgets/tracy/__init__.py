c451b7803b9802141eb0761eb1073329783a6448
Petr Hejna <Achse0@gmail.com>
2016-11-28 10:36:45 +0100

    attempt to fix tracy not found error, ignore it silently (on production there is no tracy)

--- beedriver/po/widgets/tracy/__init__.py
+++ beedriver/po/widgets/tracy/__init__.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import TimeoutException
+
 from beedriver.po.elements.button import Button
 from beedriver.po.page_object import PageObject
 from .locators import TracyLocators as Locators
@@ -8,5 +10,8 @@ class Tracy(PageObject):
         self.close_button = Button(self, Locators.CLOSE_BUTTON)
 
     def close(self):
-        self.close_button.wait_for_exist()
+        try:
+            self.close_button.wait_for_exist()
+        except TimeoutException:
+            return  # We wait if tracy appears, if not lets move on.
         self.close_button.click()
