7f01a5862d92dea9008f3bedb983181ea89bc171
Michal Brauner <michal.br@gmail.com>
2017-01-14 15:35:07 +0100

    fix too many empty lines

--- beedriver/po/engager/left_panel/my_views/__init__.py
+++ beedriver/po/engager/left_panel/my_views/__init__.py
@@ -8,8 +8,3 @@ class MyViews(PageObject):
     def get_my_view_by_name(self, view_name):
         my_views_locators = MyViewsLocators()
         return MyView(self, my_views_locators.get_locator_for_view_by_name(view_name))
-
-
-
-
-
